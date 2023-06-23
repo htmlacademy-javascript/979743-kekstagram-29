cc956922ea527a27348aa190f9744201eed84c7d
AnnaGW <a.bogoslovskaja@yandex.ru>
2023-06-24 00:29:23 +0500

    рефакторинг. переписала массивы на объекты

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,11 @@
 const formatTime = (string) => {
   // форматируем время, приводим к массиву чисел
   const stringArray = string.split(':');
-  const result = stringArray;
-  for (let i = 0; i < stringArray.length; i++) {
-    result[i] = Number(stringArray[i]);
-  }
-  return result; // возвращаем массив чисел
+  return {
+    // возвращаем объект с числами
+    hours: Number(stringArray[0]),
+    minutes: Number(stringArray[1]),
+  };
 };
 
 const compareTime = (timeStr1, timeStr2) => {
@@ -14,34 +14,27 @@ const compareTime = (timeStr1, timeStr2) => {
   const time1 = formatTime(timeStr1);
   const time2 = formatTime(timeStr2);
   //сначала часы
-  if (time1[0] > time2[0]) {
-    console.log('первое время больше, т.е. позже');
+  if (time1.hours > time2.hours) {
     return false;
-  } else if (time1[0] < time2[0]) {
-    console.log('первое время меньше, т.е. раньше');
+  } else if (time1.hours < time2.hours) {
     return true;
-  } else if (time1[0] === time2[0]) {
-    console.log('часы совпадают, надо сравнивать минуты');
-  }
+  } //else if (time1.hours === time2.hours) {
+  //}
   // теперь сравниваем минуты
-  if (time1[1] > time2[1]) {
-    console.log('первое время больше, т.е. позже');
+  if (time1.minutes > time2.minutes) {
     return false;
-  } else if (time1[1] < time2[1]) {
-    console.log('первое время меньше, т.е. раньше');
+  } else if (time1.minutes < time2.minutes) {
     return true;
-  } else if (time1[1] === time2[1]) {
-    console.log('время совпадает');
+  } else if (time1.minutes === time2.minutes) {
     return true;
   }
 };
 
 const formatMeetingDuraion = (minutes) => {
   //превращаем минуты в массив чисел: часы и минуты
-  const meetingDuration = [];
-  meetingDuration.push(Math.trunc(minutes / 60));
-  meetingDuration.push(minutes % 60);
-  //console.log('meetingDuration - ', meetingDuration);
+  const meetingDuration = {};
+  meetingDuration.hours = Math.trunc(minutes / 60);
+  meetingDuration.minutes = minutes % 60;
   return meetingDuration;
 };
 
@@ -49,15 +42,16 @@ const getMeetingEnd = (meetingStart, meetingDuration) => {
   // формируем время окончания встречи
   const formatedMeetingStart = formatTime(meetingStart);
   const formatedMeetingDuraion = formatMeetingDuraion(meetingDuration);
-  const meetingEnd = [];
-  meetingEnd.push(formatedMeetingStart[0] + formatedMeetingDuraion[0]);
-  meetingEnd.push(formatedMeetingStart[1] + formatedMeetingDuraion[1]);
-  if (meetingEnd[1] >= 60) {
-    meetingEnd[0] += 1;
-    meetingEnd[1] = meetingEnd[1] - 60;
+  const meetingEnd = {};
+  meetingEnd.hours = formatedMeetingStart.hours + formatedMeetingDuraion.hours;
+  meetingEnd.minutes =
+    formatedMeetingStart.minutes + formatedMeetingDuraion.minutes;
+  if (meetingEnd.minutes >= 60) {
+    meetingEnd.hours += 1;
+    meetingEnd.minutes = meetingEnd.minutes - 60;
   }
-
-  return meetingEnd.join(':'); // возвращаем строку
+  //return meetingEnd.join(':'); // возвращаем строку
+  return String(meetingEnd.hours) + ':' + String(meetingEnd.minutes); // возвращаем строку
 };
 
 //console.log('getMeetingEnd - ', getMeetingEnd('14:15', 50));
@@ -73,20 +67,20 @@ const isMeetengInWorkingDay = (
   // meetingDuration - number, minutes
   //проверяем, попадает ли время начала встречи в рабочий день
   if (compareTime(workingStart, meetingStart)) {
-    console.log('начало встречи в рабочее время');
+    //console.log('начало встречи в рабочее время');
   } else {
-    console.log('начало встречи раньше начала рабочего дня');
+    //console.log('начало встречи раньше начала рабочего дня');
     return false;
   }
   const meetingEnd = getMeetingEnd(meetingStart, meetingDuration);
   //проверяем, укаладывается ли время окончания встречи в рабочий день
   if (compareTime(meetingEnd, workingEnd)) {
-    console.log('встреча закончилась в рабочее время');
+    //console.log('встреча закончилась в рабочее время');
     return true;
   } else {
-    console.log('встреча закончилась позже окончания рабочиего дня');
+    //console.log('встреча закончилась позже окончания рабочиего дня');
     return false;
   }
 };
 
-console.log(isMeetengInWorkingDay('8:00', '17:30', '08:00', 900));
+//console.log(isMeetengInWorkingDay('8:0', '10:0', '8:0', 120));
