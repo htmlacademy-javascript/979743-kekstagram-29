f9d508a12fa2893f6d6e519568e5f239d6d43265
AnnaGW <a.bogoslovskaja@yandex.ru>
2023-07-22 22:25:07 +0500

    один catch в main. показ блока для ошибки (m11t1)

--- js/main.js
+++ js/main.js
@@ -1,30 +1,21 @@
 // точка входа. подключен в index.html
 //import { allPhotosData } from './data.js'; //получаем массив из 25 объектов
 // import { SERVER_URL } from './enums.js';
-import { getData } from './server.js';
+import { getData, showError } from './server.js';
+import { ErrorText } from './enums.js';
 import { renderThumbnails } from './render-thumbnails.js';
 import { addListenerThumbnailsContainer } from './on-thumbnail-click.js';
 import { addListenerForUpload } from './on-upload-click.js';
 
-// fetch(SERVER_URL)
-//   .then((response) => response.json())
-//   .then((photosData) => {
-//     renderThumbnails(photosData); // отдали данные на отрисовку
-//     addListenerThumbnailsContainer(photosData);
-//   });
-
-//надо из server вернуть промис, который загружает данные.
-//потом then и в нем функции отрисовки и прослушки
-// fetchPhotos.then((photosData) => {
-//   console.log('from then');
-//   renderThumbnails(photosData); // отдали данные на отрисовку
-//   addListenerThumbnailsContainer(photosData);
-// });
 //из server возвращаем промис, который отдает данные
 //потом then и в нем функции отрисовки и прослушки
-getData().then((photosData) => {
-  renderThumbnails(photosData); // отдали данные на отрисовку
-  addListenerThumbnailsContainer(photosData);
-});
+getData()
+  .then((photosData) => {
+    renderThumbnails(photosData); // отдали данные на отрисовку
+    addListenerThumbnailsContainer(photosData);
+  })
+  .catch(() => {
+    showError(ErrorText.GET_DATA);
+  });
 
 addListenerForUpload();
--- js/server.js
+++ js/server.js
@@ -1,20 +1,27 @@
 //загружает данные с сервера, обрабатывает ошибки; возвращает промис
 import { Url, Method, ErrorText } from './enums.js';
 
+const downloadErrorElem = document.querySelector('.download-error');
+const downloadErrorText = document.querySelector('.download-error span');
+
+const showError = (message) => {
+  downloadErrorText.textContent = message;
+  downloadErrorElem.classList.remove('hidden');
+};
+
 const load = (url, errorText, method = Method.GET, body = null) =>
-  fetch(url, { method, body })
-    .then((response) => {
-      // проверка кода ответа
-      //console.log('response - ', response.status);
-      if (!response.ok) {
-        throw new Error();
-      }
-      return response.json();
-    })
-    .catch(() => {
-      throw new Error(errorText);
-    }); // load возвращает промис
+  fetch(url, { method, body }).then((response) => {
+    // проверка кода ответа
+    //console.log('response - ', response.status);
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  }); // load возвращает промис
+// .catch(() => { // ЗАЧЕМ ОН ЗДЕСЬ??? ОШИБКУ МОЖНО ОТЛОВИТЬ В MAIN
+//   throw new Error(errorText);
+// });
 
 const getData = () => load(Url.DOWNLOAD, ErrorText.GET_DATA); // возвращает промис
 
-export { getData };
+export { getData, showError };
